8dcc543dee7ef98d23e50c151f3e20fc38d91823
Jason <jasonkwok475@gmail.com>
2026-07-26 13:42:09 -0700

    Fixed DMA to Circular for all UART and updated priority map

--- Core/Src/stm32g0xx_hal_msp.c
+++ Core/Src/stm32g0xx_hal_msp.c
@@ -280,7 +280,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_LINKDMA(huart,hdmarx,hdma_usart1_rx);
 
     /* USART1 interrupt Init */
-    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(USART1_IRQn, 2, 0);
     HAL_NVIC_EnableIRQ(USART1_IRQn);
     /* USER CODE BEGIN USART1_MspInit 1 */
 
@@ -333,7 +333,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     hdma_usart5_rx.Init.MemInc = DMA_MINC_ENABLE;
     hdma_usart5_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_usart5_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_usart5_rx.Init.Mode = DMA_NORMAL;
+    hdma_usart5_rx.Init.Mode = DMA_CIRCULAR;
     hdma_usart5_rx.Init.Priority = DMA_PRIORITY_LOW;
     if (HAL_DMA_Init(&hdma_usart5_rx) != HAL_OK)
     {
@@ -343,7 +343,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_LINKDMA(huart,hdmarx,hdma_usart5_rx);
 
     /* USART5 interrupt Init */
-    HAL_NVIC_SetPriority(USART3_4_5_6_LPUART1_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(USART3_4_5_6_LPUART1_IRQn, 2, 0);
     HAL_NVIC_EnableIRQ(USART3_4_5_6_LPUART1_IRQn);
     /* USER CODE BEGIN USART5_MspInit 1 */
 
@@ -406,7 +406,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_LINKDMA(huart,hdmarx,hdma_usart6_rx);
 
     /* USART6 interrupt Init */
-    HAL_NVIC_SetPriority(USART3_4_5_6_LPUART1_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(USART3_4_5_6_LPUART1_IRQn, 2, 0);
     HAL_NVIC_EnableIRQ(USART3_4_5_6_LPUART1_IRQn);
     /* USER CODE BEGIN USART6_MspInit 1 */
 
